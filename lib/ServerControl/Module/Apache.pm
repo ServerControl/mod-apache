3c519119b1fbb6b48b36f67f2ed1df065361bc0b
Jan Gehring <krimdomu@googlemail.com>
2011-06-09 18:53:43 +0200

    removed director and file specifications

--- lib/ServerControl/Module/Apache.pm
+++ lib/ServerControl/Module/Apache.pm
@@ -22,29 +22,6 @@ __PACKAGE__->Parameter(
    help  => { isa => 'bool', call => sub { __PACKAGE__->help; } },
 );
 
-__PACKAGE__->Directories(
-   bin      => { chmod => 0755, user => 'root', group => 'root' },
-   conf     => { chmod => 0700, user => 'root', group => 'root' },
-   htdocs   => { chmod => 0755, user => ServerControl::Args->get->{'user'}, group => ServerControl::Args->get->{'group'} },
-   error    => { chmod => 0755, user => ServerControl::Args->get->{'user'}, group => ServerControl::Args->get->{'group'} },
-   logs     => { chmod => 0755, user => ServerControl::Args->get->{'user'}, group => ServerControl::Args->get->{'group'} },
-   run      => { chmod => 0755, user => ServerControl::Args->get->{'user'}, group => ServerControl::Args->get->{'group'} },
-   tmp      => { chmod => 0755, user => ServerControl::Args->get->{'user'}, group => ServerControl::Args->get->{'group'} },
-
-   'conf/httpd-conf.d'     => { chmod => 0700, user => 'root', group => 'root' },
-   'conf/vhost-conf.d'     => { chmod => 0700, user => 'root', group => 'root' },
-);
-
-__PACKAGE__->Files(
-   'bin/httpd-' . __PACKAGE__->get_name  => { link => ServerControl::Schema->get('httpd') },
-
-   'modules'                             => { link => ServerControl::Schema->get('modules') },
-
-   'conf/magic'                          => { link => ServerControl::Schema->get('magic') },
-   'conf/mime.types'                     => { link => ServerControl::Schema->get('mime.types') },
-   'conf/httpd.conf'                     => { call => sub { ServerControl::Template->parse(@_); } },
-);
-
 sub help {
    my ($class) = @_;
 
